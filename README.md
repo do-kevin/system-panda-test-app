5e4b8a69c50d6d0137f3762d78de7de81480e5dc
Kevin Do <do.kevin.vo@gmail.com>
2023-07-10 17:47:59 -0700

    :tada: initial setup to use system-panda

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # system-panda-test-app
+
+Run with `npm`, not `yarn` or `pnpm`.
+
+## Step 1
+Download https://github.com/serhankileci/system-panda repository
+
+## Step 2
+
+Move `system-panda-test-app` into the same directory as `system-panda` or go into `package.json` and change the URL.
+
+## Step 3
+
+```bash
+npm install --install-links
+```
---
+++ src/app.ts
@@ -0,0 +1,181 @@
+console.log("---app starting---");
+import "dotenv/config";
+
+import SystemPanda from "system-panda";
+console.log("PORT: ", process.env.PORT);
+
+const localhost = `http://localhost:${process.env.PORT}`;
+
+SystemPanda({
+  content: {
+    // define your data models
+    collections: {
+      student: {
+        id: {
+          name: "id",
+          type: "uuid",
+        },
+        fields: {
+          name: {
+            type: "String",
+          },
+          relation_classroom: {
+            type: "relation",
+            many: true,
+            ref: "classroom.id",
+          },
+        },
+      },
+      classroom: {
+        hooks: {
+          beforeOperation: [],
+          modifyInput: [],
+          validateInput: [],
+          afterOperation: [],
+        },
+        fields: {
+          name: {
+            type: "String",
+            required: true,
+          },
+          relation_student: {
+            type: "relation",
+            many: true,
+            ref: "student.id",
+          },
+        },
+      },
+      song: {
+        fields: {
+          title: {
+            type: "String",
+            required: true,
+          },
+          relation_album: {
+            type: "relation",
+            ref: "album.relation_song",
+            many: false,
+          },
+        },
+      },
+      album: {
+        slug: "record",
+        fields: {
+          relation_song: {
+            type: "relation",
+            ref: "song.relation_album",
+            many: true,
+          },
+          year: {
+            type: "number",
+            subtype: "Int",
+            required: true,
+          },
+          title: {
+            type: "String",
+            required: true,
+            unique: true,
+            index: true,
+          },
+          dateCreated: {
+            type: "DateTime",
+            defaultValue: {
+              kind: "now",
+            },
+          },
+          mJson: {
+            type: "Json",
+            defaultValue: JSON.stringify({ hello: "world" }),
+          },
+        },
+        webhooks: [
+          {
+            api: localhost + "/webhook",
+            name: "All Operations",
+
+            // pick which operations should trigger the webhook
+            onOperation: ["create"],
+          },
+        ],
+        hooks: {
+          beforeOperation: [
+            async ({ ctx, operation, existingData, inputData }) => {
+              // cause side-effect
+
+              const userType = ctx.sessionData?.userType;
+              const isUserAndReadOp =
+                userType === "user" && operation === "read";
+              const isAdmin = userType === "admin";
+
+              return isUserAndReadOp || isAdmin;
+            },
+          ],
+          modifyInput: [
+            async ({ ctx, operation, existingData, inputData }) => {
+              // modify request body
+              inputData.title = "";
+
+              return inputData;
+            },
+          ],
+          validateInput: [
+            async ({ ctx, operation, existingData, inputData }) => {
+              // some kind of validation
+              if (!/\s/.test(inputData)) {
+                throw "Validation error!";
+              }
+
+              return inputData;
+            },
+          ],
+          afterOperation: [
+            async ({ ctx, operation, existingData, inputData }) => {
+              // cause side-effect
+            },
+          ],
+        },
+      },
+    },
+  },
+  settings: {
+    port: Number(process.env.PORT),
+    db: {
+      URI: process.env.DATABASE_URL!,
+    },
+    extendServer: async (app, ctx) => {
+      // add custom server logic
+
+      app.post("/webhook", (req, res, next) => {
+        /*
+          when, for example, a POST request is received with the following request body:
+          {
+            event: 'create',
+            collection: 'album',
+            data: { beforeCreate: null, afterCreate: [ [Object] ] },
+            timestamp: '2023-06-16T20:26:31.485Z'
+          }
+          */
+
+        res.sendStatus(200);
+      });
+    },
+
+    authSession: {
+      options: {
+        secret: String(process.env.SECRET),
+      },
+      initFirstAuth: {
+        email: "admin@system-panda.com",
+        password: "1234",
+      },
+    },
+
+    /*
+      popular middlewares enabled by default
+      configure their options or turn them off
+    */
+    defaultMiddlewares: {
+      morgan: false,
+    },
+  },
+});
